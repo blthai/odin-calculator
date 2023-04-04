7d526ce44f406754ff0f43fa851c3fec0b6deaf4
blthai <39754216+blthai@users.noreply.github.com>
2023-04-04 14:44:20 -0700

    add event listeners to buttons with digits to update the display

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-let firstNumber = 0;
-let secondNumber = 0;
+let firstNumber = '';
+let secondNumber = '';
 let operator;
+let operatorIsPressed = false;
+
+console.log(firstNumber+'hi')
 
 function add(number1, number2){
   return number1 + number2;
@@ -32,3 +35,21 @@ function operate(operator, number1, number2){
     return divide(number1, number2);
   }
 }
+
+const digitButtons = document.querySelectorAll('.digit');
+digitButtons.forEach((button) => button.addEventListener('click', populateDisplay));
+const display = document.querySelector('textarea');
+
+function populateDisplay(event){
+  if(operatorIsPressed){
+    secondNumber = secondNumber + this.textContent;
+    display.textContent = secondNumber;
+  }
+  else{
+    firstNumber = firstNumber + this.textContent;
+    display.textContent = firstNumber;
+  }
+}
+
+
+
