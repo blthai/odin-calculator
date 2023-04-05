c67c8b5c05b4a7f72ecd2e9309cca66a9836d5c3
blthai <39754216+blthai@users.noreply.github.com>
2023-04-04 18:21:58 -0700

    add processing for operator and equality button clicks

--- script.js
+++ script.js
@@ -1,46 +1,51 @@
 let firstNumber = '';
 let secondNumber = '';
-let operator;
+let operator = '';
 let operatorIsPressed = false;
-
-console.log(firstNumber+'hi')
+let resultExists = false;
+const digitButtons = document.querySelectorAll('.digit');
+const display = document.querySelector('textarea');
+const equalityButton = document.querySelector('.equality');
+const operatorButtons = document.querySelectorAll('.operator');
 
 function add(number1, number2){
-  return number1 + number2;
+  return Number(number1) + Number(number2);
 }
 
 function subtract(number1, number2){
-  return number1 - number2;
+  return Number(number1) - Number(number2);
 }
 
 function multiply(number1, number2){
-  return number1 * number2;
+  return Number(number1) * Number(number2);
 }
 
 function divide(number1, number2){
-  return number1 / number2;
+  return Number(number1) / Number(number2);
 }
 
-function operate(operator, number1, number2){
+function operate(){
   if(operator === '+'){
-    return add(number1, number2);
+    return add(firstNumber, secondNumber);
   }
   else if(operator === '-'){
-    return subtract(number1, number2);
+    return subtract(firstNumber, secondNumber);
   }
   else if(operator === '*'){
-    return multiply(number1, number2);
+    return multiply(firstNumber, secondNumber);
   }
   else if(operator === '/'){
-    return divide(number1, number2);
+    return divide(firstNumber, secondNumber);
   }
 }
 
-const digitButtons = document.querySelectorAll('.digit');
-digitButtons.forEach((button) => button.addEventListener('click', populateDisplay));
-const display = document.querySelector('textarea');
-
 function populateDisplay(event){
+  // if we want to use the result after a calculate this firstnumber = "" is deleting the result, I DECIDED TO SET RESULT EXISTS = FALSE IN ASSIGN OPERATOR AS WE HAVE CHOSEN TO CONTINUE USIONG THE RESULT AT THAT POINT INSTEAD OF ENTERING
+  // A NEW VALUE
+  if(resultExists){
+    firstNumber = '';
+    resultExists = false;
+  }
   if(operatorIsPressed){
     secondNumber = secondNumber + this.textContent;
     display.textContent = secondNumber;
@@ -51,5 +56,33 @@ function populateDisplay(event){
   }
 }
 
+function assignOperator(event){
+  if(resultExists){
+    resultExists = false;
+  }
+  if(operatorIsPressed){
+    display.textContent = operate();
+    firstNumber = operate();
+    secondNumber = '';
+  }
+  else{
+    operatorIsPressed = true;
+  }
+  operator = this.textContent;
+}
 
+function calculate(){
+  if(secondNumber===''){
+    return;
+  }
+  display.textContent = operate();
+  firstNumber = operate();
+  secondNumber = '';
+  operator = '';
+  operatorIsPressed = false;
+  resultExists = true;
+}
 
+digitButtons.forEach((button) => button.addEventListener('click', populateDisplay));
+equalityButton.addEventListener('click', calculate);
+operatorButtons.forEach(((button) => button.addEventListener('click', assignOperator)));
