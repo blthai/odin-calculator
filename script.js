3309614b206a1750c19eb02defe45630ab1e9b18
blthai <39754216+blthai@users.noreply.github.com>
2023-04-05 18:18:21 -0700

    add delete interaction that affects the display and numbers used in operations

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-let firstNumber = '';
+let firstNumber = '0';
 let secondNumber = '';
 let operator = '';
 let operatorIsPressed = false;
 let resultExists = false;
 let decimalIsPressed = false;
 let originalFirstNumber = '';
+let tempResultExists = false;
 const digitButtons = document.querySelectorAll('.digit');
 const display = document.querySelector('.display');
 const equalityButton = document.querySelector('.equality');
 const operatorButtons = document.querySelectorAll('.operator');
 const clearButton = document.querySelector('.clear');
+const delButton = document.querySelector('.delete');
 
 function add(number1, number2){
   return Number(number1) + Number(number2);
@@ -50,6 +52,9 @@ function populateDisplay(event){
     firstNumber = '';
     resultExists = false;
   }
+  if(tempResultExists){
+    tempResultExists = false;
+  }
   if(this.textContent === '.'){
     if(decimalIsPressed){
       return;
@@ -57,13 +62,20 @@ function populateDisplay(event){
     decimalIsPressed = true;
   }
   if(operatorIsPressed){
+    if(secondNumber === '' && this.textContent === '0'){
+      return;
+    }
     secondNumber = secondNumber + this.textContent;
     display.textContent = secondNumber;
   }
   else{
-    firstNumber = firstNumber + this.textContent;
+    if(firstNumber === '0'){
+      firstNumber = this.textContent
+    }
+    else{
+      firstNumber = firstNumber + this.textContent;
+    }
     display.textContent = firstNumber;
-    console.log(display.textContent)
   }
 }
 
@@ -85,6 +97,7 @@ function assignOperator(event){
     display.textContent = Number(Number(operate().toFixed(9)));
     firstNumber = operate();
     secondNumber = '';
+    tempResultExists = true;
   }
   else if(firstNumber === ''){
     return;
@@ -95,6 +108,7 @@ function assignOperator(event){
 
 //consider creating a variable to store the first duplicate equals generated number the ncheck if its empty and if not empty and second number is emptywere going to use the calculate
 function calculate(){
+  
   if(firstNumber === '' || (secondNumber === '' && operatorIsPressed === false)){
     return;
   }
@@ -127,8 +141,33 @@ function clearCalculator(event){
   display.textContent='0';
 }
 
+function backspace(){
+  if(resultExists || (!firstNumber && !secondNumber || tempResultExists) ){
+    return;
+  }
+  else if(secondNumber){
+    secondNumber = secondNumber.substring(0, secondNumber.length - 1);
+    if(!secondNumber){
+      display.textContent = '0';
+      return;
+    }
+    display.textContent = secondNumber;
+  }
+  else if(display.textContent === '0' && firstNumber!==''){
+    return;
+  }
+  else{
+    firstNumber = firstNumber.substring(0, firstNumber.length - 1);
+    if(!firstNumber){
+      display.textContent = '0';
+      return;
+    }
+    display.textContent = firstNumber;
+  }
+}
 
 digitButtons.forEach((button) => button.addEventListener('click', populateDisplay));
 equalityButton.addEventListener('click', calculate);
 operatorButtons.forEach(((button) => button.addEventListener('click', assignOperator)));
 clearButton.addEventListener('click', clearCalculator);
+delButton.addEventListener('click', backspace);
